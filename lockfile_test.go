53477b62859bd445b36e9161c86b689e7d05e86b
taskie <t@skie.jp>
2018-09-20 23:45:12 +0900

    add tests for lockfile

--- lockfile_test.go
+++ lockfile_test.go
@@ -10,10 +10,18 @@ func TestTryToLockFile(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	err = lockFile.TryToLock()
+	if err == nil {
+		t.Fatal("TryToLock (twice): must fail")
+	}
 	defer func() {
 		err := lockFile.TryToUnlock()
 		if err != nil {
 			t.Fatal(lockFile.Error())
+		}
+		err = lockFile.TryToUnlock()
+		if err == nil {
+			t.Fatal("TryToUnlock (twice): must fail")
 		}
 	}()
 }
