bef22bc13c001afb962821fd09652a507a2b7565
taskie <t@skie.jp>
2020-02-23 15:57:57 +0900

    add colivec

---
+++ cmd/colivec/main.go
@@ -0,0 +1,81 @@
+package main
+
+//go:generate statik -f -src templates -include GNUmakefile,_gitignore
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"text/template"
+
+	"github.com/rakyll/statik/fs"
+	"github.com/spf13/pflag"
+	"go.uber.org/zap"
+
+	_ "github.com/taskie/ose/cmd/colivec/statik"
+)
+
+type templateData struct {
+	CmdNames []string
+}
+
+func main() {
+	log, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+
+	pout := pflag.StringP("out", "o", ".", "output directory")
+	pforce := pflag.BoolP("force", "f", false, "force mode")
+	pflag.Parse()
+	cmdNames := pflag.Args()
+
+	staticFS, err := fs.New()
+	if err != nil {
+		log.Fatal("can't load fs", zap.Error(err))
+	}
+	m := map[string]string{
+		"_gitignore":  ".gitignore",
+		"GNUmakefile": "",
+	}
+	tmpl := template.New("")
+	for k := range m {
+		f, err := staticFS.Open("/" + k)
+		if err != nil {
+			log.Fatal("can't open", zap.Error(err))
+		}
+		bs, err := ioutil.ReadAll(f)
+		if err != nil {
+			log.Fatal("can't read", zap.Error(err))
+		}
+		_, err = tmpl.New(k).Parse(string(bs))
+		if err != nil {
+			log.Fatal("can't parse", zap.Error(err))
+		}
+	}
+	for k, v := range m {
+		filename := v
+		if filename == "" {
+			filename = k
+		}
+		outpath := filepath.Join(*pout, filename)
+		if !*pforce {
+			_, err = os.Stat(outpath)
+			if err == nil {
+				log.Info("exists", zap.String("outpath", outpath))
+				continue
+			}
+		}
+		of, err := os.Create(outpath)
+		if err != nil {
+			log.Fatal("can't create", zap.Error(err))
+		}
+		defer of.Close()
+		err = tmpl.ExecuteTemplate(of, k, &templateData{
+			CmdNames: cmdNames,
+		})
+		if err != nil {
+			log.Fatal("can't execute", zap.Error(err))
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -7,11 +7,17 @@ require (
 	github.com/mattn/go-colorable v0.1.4
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/rakyll/statik v0.1.6
 	github.com/spf13/afero v1.2.2
 	github.com/spf13/cobra v0.0.6
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.6.2
+	go.uber.org/atomic v1.5.1 // indirect
+	go.uber.org/multierr v1.4.0 // indirect
 	go.uber.org/zap v1.14.0
+	golang.org/x/lint v0.0.0-20200130185559-910be7a94367 // indirect
 	golang.org/x/sys v0.0.0-20200219091948-cb0a6d8edb6c // indirect
 	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20200221224223-e1da425f72fd // indirect
+	honnef.co/go/tools v0.0.1-2020.1.3 // indirect
 )
